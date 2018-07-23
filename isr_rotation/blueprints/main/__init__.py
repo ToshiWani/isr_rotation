6218534cc5d7e71bbd961ac845cbf6735d78c6cc
tkunisada <tkunisada@infragistics.com>
2018-07-22 21:36:40 -0400

    Deleted and moved files to start over

---
+++ isr_rotation/blueprints/main/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('main', __name__, template_folder='templates')
+
+
+@bp.route('/')
+def home():
+    return render_template('main/index.html')
+
+
---
+++ run.bat
@@ -0,0 +1,5 @@
+:: Batch file for Windows
+
+set FLASK_APP=isr_rotation
+set FLASK_DEBUG=true
+python -m flask run
